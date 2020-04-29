6353e3b6cbb97db95b3fb4359b14c172b32f91e8
draxxxeus <luckysudeep@gmail.com>
2020-04-29 16:10:14 +0530

    Register models in the admin page

--- virtualclassroom/app/admin.py
+++ virtualclassroom/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course, Lecture, Resource, School, User
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Lecture)
+admin.site.register(Resource)
+admin.site.register(School)
+admin.site.register(User)
