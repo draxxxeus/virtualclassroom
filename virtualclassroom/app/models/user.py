1bca0114197c6e05606cf21ee537b0806136d9dc
draxxxeus <luckysudeep@gmail.com>
2020-04-29 16:10:59 +0530

    user model - set school and standard and null=True

--- virtualclassroom/app/models/user.py
+++ virtualclassroom/app/models/user.py
@@ -7,5 +7,5 @@ from .school import School
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone = models.CharField(max_length=14)
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
-    standard = models.CharField(max_length=14)
+    school = models.ForeignKey(School, on_delete=models.CASCADE, default=None, null=True)
+    standard = models.CharField(max_length=14, default=None, null=True)
