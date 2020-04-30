732fcd99b28bf63e22fb93c2c53c9098c800dda2
draxxxeus <luckysudeep@gmail.com>
2020-04-30 20:34:07 +0530

    Define user roles - superadmin, faculty and student

--- virtualclassroom/app/models/user.py
+++ virtualclassroom/app/models/user.py
@@ -5,7 +5,14 @@ from .school import School
 
 
 class User(AbstractUser):
+    USER_ROLES = [
+            ('SU', 'school_admin'),
+            ('RW', 'faculty'),
+            ('RO', 'student')
+    ]
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone = models.CharField(max_length=14)
     school = models.ForeignKey(School, on_delete=models.CASCADE, default=None, null=True)
     standard = models.CharField(max_length=14, default=None, null=True)
+    role = models.CharField(max_length=2, choices=USER_ROLES, default=None, null=True)
