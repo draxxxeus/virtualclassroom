cf73a26f9c8b65f9e1a480c99441b73f8617b4b3
draxxxeus <luckysudeep@gmail.com>
2020-05-01 23:18:26 +0530

    resource model - fix field name to media and allow empty values for some fields

--- virtualclassroom/app/models/resource.py
+++ virtualclassroom/app/models/resource.py
@@ -12,11 +12,11 @@ class Resource(BaseModel):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=124)
+    media = models.FileField(upload_to='uploads/')
     lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
     type = models.CharField(max_length=1, choices=RESOURCE_CHOICES)
-    link = models.URLField(max_length=512)
-    publish_on = models.DateTimeField()
-    complete_by = models.DateTimeField()
+    link = models.URLField(max_length=512, default=None, null=True, blank=True)
+    publish_on = models.DateTimeField(default=None, null=True, blank=True)
+    complete_by = models.DateTimeField(default=None, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
