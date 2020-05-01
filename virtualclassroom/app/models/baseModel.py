99f41edad1d2ef3c4927df8378027abe9b9f85b1
draxxxeus <luckysudeep@gmail.com>
2020-05-01 23:15:06 +0530

    Return id by default for any model

--- virtualclassroom/app/models/baseModel.py
+++ virtualclassroom/app/models/baseModel.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
     def __str__(self):
-        if self.__getattribute__('name'):
-            return self.__getattribute__('name') #TODO: test if __getattributes__ or __getitem__ is useful
+        if self.__getattribute__('id'):
+            return str(self.__getattribute__('id')) #TODO: test if __getattributes__ or __getitem__ is useful
 
     # this is to make the class abstract so that django do not create table for it
     class Meta:
