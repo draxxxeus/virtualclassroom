30c9f27948d5aff5c343e43b12c253f9cbf1afb8
draxxxeus <luckysudeep@gmail.com>
2020-05-02 11:04:34 +0530

    urls - include media path

--- virtualclassroom/app/urls.py
+++ virtualclassroom/app/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     path('pricing/', views.pricing, name='pricing'),
     path('logout/', views.logout, name='logout'),
     path('upload/', views.upload, name='upload'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
