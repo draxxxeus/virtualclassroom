0bcdbb1d60adbf12505dd96670b9dec77f12b399
draxxxeus <luckysudeep@gmail.com>
2020-05-03 13:56:26 +0530

    show textbook (if available) on lecture page

--- virtualclassroom/app/models/course.py
+++ virtualclassroom/app/models/course.py
@@ -2,6 +2,9 @@ import uuid
 from .baseModel import *
 
 
+def get_textbook_path(instance, filename):
+	return 'courses/{0}/{1}'.format(instance.id, filename)
+
 class Course(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     school = models.ForeignKey('School', on_delete=models.CASCADE)
@@ -9,6 +12,7 @@ class Course(BaseModel):
     standard = models.CharField(max_length=14)
     subject = models.CharField(max_length=30)
     teacher = models.ForeignKey('User', on_delete=models.CASCADE)
+    textbook = models.FileField(upload_to=get_textbook_path, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
