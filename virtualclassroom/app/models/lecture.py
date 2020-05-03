8b460aacbc1b1a5ecbd5dde3e557c836d1ddf51c
draxxxeus <luckysudeep@gmail.com>
2020-05-03 13:56:50 +0530

    lecture model - set publish_on as null=True

--- virtualclassroom/app/models/lecture.py
+++ virtualclassroom/app/models/lecture.py
@@ -13,7 +13,7 @@ class Lecture(BaseModel):
     index = models.PositiveIntegerField(default=1, null=False)
     course = models.ForeignKey('Course', on_delete=models.CASCADE)
     teacher = models.ForeignKey('User', on_delete=models.CASCADE)
-    publish_on = models.DateTimeField(default=now, blank=True)
+    publish_on = models.DateTimeField(default=now, null=True, blank=True)
     complete_by = models.DateTimeField(default=None, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
