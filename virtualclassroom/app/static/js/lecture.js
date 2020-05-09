adbaeaa541b6aec4320865275b2656756a148b02
draxxxeus <luckysudeep@gmail.com>
2020-05-09 13:57:33 +0530

    Show readable date on discussion page

--- virtualclassroom/app/static/js/lecture.js
+++ virtualclassroom/app/static/js/lecture.js
@@ -7,9 +7,23 @@ $("#new_comment").on("submit", function (event) {
         data: frmValues
     })
     .done(function (data) {
-        html_data = "<small>"+data['user']+"</small>"
-        html_data += "<small class='float-right'>"+data['date_created']+"</small>"
-        html_data += "<li>"+data['comment']+"</li>"
+        dt = new Date(data['date_created']);
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        comment_date = [];
+        comment_date.push(months[dt.getMonth()]);
+        comment_date.push(dt.getDay() + ",");
+        comment_date.push(dt.getFullYear());
+        hours = dt.getHours();
+        suffix = 'a.m.';
+        if (hours > 12) {
+          hours = hours - 12;
+          suffix = 'p.m.';
+        }
+        comment_date.push(hours + ":" + dt.getMinutes());
+        comment_date.push(suffix);
+        html_data = "<small>"+data['user']+"</small>";
+        html_data += "<small class='float-right'>"+comment_date.join(' ')+"</small>";
+        html_data += "<li>"+data['comment']+"</li>";
         $("ul#discussions").prepend(html_data).hide().fadeIn('slow');
         $("#new_comment")[0].reset();
     })
