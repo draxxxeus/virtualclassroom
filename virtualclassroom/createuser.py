614335b7bb9770f47ffa3e60ab9a2478ba3ea098
Rahul Shaw <rahulshaw.2009@gmail.com>
2020-05-10 11:01:47 +0530

    create objects lecture

--- virtualclassroom/createuser.py
+++ virtualclassroom/createuser.py
@@ -1,14 +1,14 @@
 import factory
 from factory.django import DjangoModelFactory
-from app.models import User
-from app.models import School
-from app.models import Course
-from datetime import datetime
+from app.models import User, School, Course, Lecture, Resource
+from datetime import datetime, timedelta
 
 teacher_template = 'testteacher{}@oneschool.pw'
 student_template = 'teststudent{}@oneschool.pw'
 school_name_template = 'Test School {}'
 school_email_template = 'testschool{}@oneschool.pw'
+chapter_name_template = 'testchapter{}'
+course_topic_template = 'testchaptertopic{}'
 password = 'pbkdf2_sha256$180000$3EPpD1Z7Nn7Z$sEq6A4SWttHmGvoFdvQSLWY0VHHFU7TKJ/PinNXlj6A='  # decoded: 1234
 
 
@@ -56,11 +56,21 @@ class CourseFactory(DjangoModelFactory):
         model = Course
 
 
+class LectureFactory(DjangoModelFactory):
+    chapter = factory.Sequence(chapter_name_template.format)
+    topic = factory.Sequence(course_topic_template.format)
+    complete_by = datetime.now() + timedelta(weeks=104)
+
+    class Meta:
+        model = Lecture
+
+
 def createObjects():
     school = SchoolFactory()
     teacher = TeacherFactory(school=school)
     student1 = StudentFactory(school=school, standard='X')
     student2 = StudentFactory(school=school, standard='XI')
-    course = CourseFactory(teacher=teacher, standard='X', school=school)
-    course = CourseFactory(teacher=teacher, standard='XI', school=school)
-
+    course1 = CourseFactory(teacher=teacher, standard='X', school=school)
+    lecture11 = LectureFactory(teacher=teacher, course=course1)
+    course2 = CourseFactory(teacher=teacher, standard='XI', school=school)
+    lecture21 = LectureFactory(teacher=teacher, course=course2)
