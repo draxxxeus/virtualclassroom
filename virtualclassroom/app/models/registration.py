9592060fa2b4028ad8363bf54d30965a6e667358
draxxxeus <luckysudeep@gmail.com>
2020-05-12 13:29:40 +0530

    Add model 'registration'

---
+++ virtualclassroom/app/models/registration.py
@@ -0,0 +1,22 @@
+import uuid
+
+from django.db import models
+
+from .baseModel import BaseModel
+
+
+class Registration(BaseModel):
+    USER_ROLES = [
+            ('SU', 'institute_admin'),
+            ('RW', 'faculty'),
+            ('RO', 'student')
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=14, default=None, null=True, blank=True)  # noqa: E501
+    institution = models.ForeignKey('Institution', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    standard = models.ForeignKey('Standard', on_delete=models.CASCADE, default=None, null=True, blank=True)  # noqa: E501
+    role = models.CharField(max_length=2, choices=USER_ROLES, default='RO')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
