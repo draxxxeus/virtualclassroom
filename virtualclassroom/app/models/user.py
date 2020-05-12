6242e9d442f7be218c0a66eeb64db39714ec496b
draxxxeus <luckysudeep@gmail.com>
2020-05-12 13:51:00 +0530

    user model - remove models that are now in registration model

--- virtualclassroom/app/models/user.py
+++ virtualclassroom/app/models/user.py
@@ -5,14 +5,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    USER_ROLES = [
-            ('SU', 'institute_admin'),
-            ('RW', 'faculty'),
-            ('RO', 'student')
-    ]
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone = models.CharField(max_length=14, default=None, null=True, blank=True)
-    institution = models.ForeignKey('Institution', on_delete=models.CASCADE, default=None, null=True)
-    standard = models.CharField(max_length=14, default=None, null=True, blank=True)
-    role = models.CharField(max_length=2, choices=USER_ROLES, default='RO')
