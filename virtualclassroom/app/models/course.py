818676a2136c719da70c262f86cbaccdcf16ae80
draxxxeus <luckysudeep@gmail.com>
2020-05-12 13:49:13 +0530

    course model - use standard as a foreign key

--- virtualclassroom/app/models/course.py
+++ virtualclassroom/app/models/course.py
@@ -11,12 +11,11 @@ def get_textbook_path(instance, filename):
 
 class Course(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    institution = models.ForeignKey('Institution', on_delete=models.CASCADE)
-    academic_year = models.CharField(max_length=14, default=None, null=True, blank=True)
-    standard = models.CharField(max_length=14)
     subject = models.CharField(max_length=30)
+    standard = models.ForeignKey('Standard', on_delete=models.CASCADE)
     teacher = models.ForeignKey('User', on_delete=models.CASCADE)
-    textbook = models.FileField(upload_to=get_textbook_path, null=True, blank=True)
+    academic_year = models.CharField(max_length=14, default=None, null=True, blank=True)  # noqa: E501
+    textbook = models.FileField(upload_to=get_textbook_path, null=True, blank=True)  # noqa: E501
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
