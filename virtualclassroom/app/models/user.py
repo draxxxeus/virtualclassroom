e39d2987113afe7d787f455d61d4942c3913a35d
draxxxeus <luckysudeep@gmail.com>
2020-05-12 13:55:38 +0530

    user model - supress linter error about line length too long

--- virtualclassroom/app/models/user.py
+++ virtualclassroom/app/models/user.py
@@ -6,4 +6,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone = models.CharField(max_length=14, default=None, null=True, blank=True)
+    phone = models.CharField(max_length=14, default=None, null=True, blank=True)  # noqa: E501
