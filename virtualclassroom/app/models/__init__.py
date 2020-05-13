ae266e68ffae8d275bdf705a4b2c77e259f02fa4
draxxxeus <luckysudeep@gmail.com>
2020-05-13 23:51:16 +0530

    models __init__ - import models alphabetically

--- virtualclassroom/app/models/__init__.py
+++ virtualclassroom/app/models/__init__.py
@@ -1,9 +1,9 @@
-from .standard import Standard
-from .registration import Registration
 from .course import Course  # noqa: F401
+from .discussion import Discussion  # noqa: F401
+from .institution import Institution  # noqa: F401
 from .lecture import Lecture  # noqa: F401
 from .notifications import Notifications  # noqa: F401
+from .registration import Registration  # noqa: F401
 from .resource import Resource  # noqa: F401
-from .institution import Institution  # noqa: F401
+from .standard import Standard  # noqa: F401
 from .user import User  # noqa: F401
-from .discussion import Discussion  # noqa: F401
