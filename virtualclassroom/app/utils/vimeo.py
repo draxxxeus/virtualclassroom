d6d298fdcfaf2992f7953aac70ab30cd79281a6a
draxxxeus <luckysudeep@gmail.com>
2020-05-13 23:50:35 +0530

    utils:vimeo - cleanup

--- virtualclassroom/app/utils/vimeo.py
+++ virtualclassroom/app/utils/vimeo.py
@@ -13,14 +13,14 @@ class Vimeo:
             secret=creds.client_secret)
         # embed settings are set via embed presets on vimeo settings UI
 
-
     def test(self, uri='/tutorial'):
         response = self.client.get(uri)
         print(response.json())
 
     def patch(self, uri, settings):
         """
-        # settings description can be seen here: https://developer.vimeo.com/api/reference/videos#edit_video
+        settings description can be seen here:
+        https://developer.vimeo.com/api/reference/videos#edit_video
         example settings:
 
         :param uri: str eg: /videos/1234567
@@ -38,16 +38,10 @@ class Vimeo:
 
         self.client.patch(uri, data=self.video_settings)
 
-    def upload(self, file_path: str, name: str = 'testname', description: str = 'destdescription'):
+    def upload(self, file_path: str, name: str = 'testname', description: str = 'destdescription'):  # noqa: E501
 
         settings = dict(name=name, description=description)
         uri = self.client.upload(file_path, data=settings)
 
         response = self.client.get(uri + '?fields=link').json()
         return response['link']
-
-
-if __name__ == '__main__':
-    v = Vimeo()
-    v.test()
-    v.upload('/Users/rshaw/Movies/test_movie.mp4')
