5ccc7b584de2d9eb5c77061d2a10a22bcabc79c1
draxxxeus <luckysudeep@gmail.com>
2020-05-14 23:41:57 +0530

    Add models [registration, standard] to admin page

--- virtualclassroom/app/admin.py
+++ virtualclassroom/app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Course, Discussion, Institution, Lecture, Resource, User
+from .models import Course, Discussion, Institution, Lecture, Registration, Resource, Standard, User  # noqa: E501
 
 admin.site.register(Course)
 admin.site.register(Discussion)
 admin.site.register(Institution)
 admin.site.register(Lecture)
+admin.site.register(Registration)
 admin.site.register(Resource)
+admin.site.register(Standard)
 admin.site.register(User)
